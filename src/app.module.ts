e796321f9ddbe284efd75e455760e242fb8bd1ba
juanxo13597 <juanxo97master@gmail.com>
2023-02-15 13:56:13 +0100

    conexion mysql, install compodoc and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'juanjosenavarroperea',
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: true, // ! true in dev, false in prod
+        autoLoadEntities: true, // ! true in dev, false in prod
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 10 })
+  name: string;
+
+  @Column({ length: 20 })
+  surname: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+}
