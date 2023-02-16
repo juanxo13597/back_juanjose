d1dbb41a5fcc36ef5484aaccf9e2f26983e189e4
juanxo13597 <juanxo97master@gmail.com>
2023-02-16 14:11:02 +0100

    change port

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { AppService } from './app.service';
       useFactory: () => ({
         type: 'mysql',
         host: process.env.DB_HOST || 'localhost',
-        port: 3306,
+        port: parseInt(process.env.DB_PORT) || 3306,
         username: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_DB || 'juanjosenavarroperea',
