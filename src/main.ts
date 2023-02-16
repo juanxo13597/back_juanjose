7a0270943bee35ae5ef6bc1b8c80db494f7b327e
juanxo13597 <juanxo97master@gmail.com>
2023-02-16 14:16:09 +0100

    change port app

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api/documentation', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
